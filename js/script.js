5eba80b30c2063cc5752bbf04018d0227bcc3982
Zile10 <johnnoad04@gmail.com>
2022-12-10 15:50:40 +0200

    Sort Done Products Updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let productItems;
 
-
 class Product{
   constructor(config) {
     this.itemName = config.itemName
@@ -13,47 +12,113 @@ class Product{
 if (!localStorage.getItem("products") || localStorage.getItem("products") == '[]') {
   productItems = [
     {
-      itemName: 'Polka Dot Socks',
-      price: 40,
+      itemName: "men's cotton tshirt blue",
+      price: 199,
       stock: 100,
-      imageSrc: './assets/images/products/happy-socks-big-polka-dots-blue-green-red-white.jpg',
+      color: 'blue',
+      gender: 'male',
+      imageSrc: 'https://i.postimg.cc/ncCCNvGM/mens-cotton-tshirt-blue.jpg',
       description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
     },
     {
-      itemName: 'Cloud Socks',
-      price: 40,
+      itemName: "men's cotton tshirt white",
+      price: 199,
       stock: 100,
-      imageSrc: './assets/images/products/happy-socks-blue-cloud.jpg',
+      color: 'white', 
+      gender: 'male',
+      imageSrc: 'https://i.postimg.cc/rFmrM6MV/mens-cotton-tshirt-white.jpg',
       description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
     },
     {
-      itemName: 'Cat Socks',
-      price: 40,
+      itemName: "men's leather jacket black",
+      price: 2699,
       stock: 100,
-      imageSrc: './assets/images/products/happy-socks-cats-black.jpg',
+      color: 'black',
+      gender: 'male',
+      imageSrc: 'https://i.postimg.cc/pTRmS4st/mens-leather-jacket-black.jpg',
       description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
     },
     {
-      itemName: 'Striped Socks',
-      price: 40,
+      itemName: "men's leather jacket brown",
+      price: 2499,
       stock: 100,
-      imageSrc: './assets/images/products/happy-socks-stripe-socks.jpg',
+      color: 'brown',
+      gender: 'male',
+      imageSrc: 'https://i.postimg.cc/FRpYXp6k/mens-leather-jacket-brown.jpg',
       description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
     },
     {
-      itemName: 'Banana Socks',
-      price: 40,
+      itemName: "women's denim top blue",
+      price: 129,
       stock: 100,
-      imageSrc: './assets/images/products/happy-socks-cyan-banana.webp',
+      color: 'blue',
+      gender: 'female',
+      imageSrc: 'https://i.postimg.cc/8P0rCyzh/womens-denim-top-blue.jpg',
       description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
     },
     {
-      itemName: 'Frog Socks',
-      price: 40,
+      itemName: "women's leather jacket black",
+      price: 2699,
       stock: 100,
-      imageSrc: './assets/images/products/happy-socks-frogs-black-yellow.jpg',
+      color: 'black',
+      gender: 'female',
+      imageSrc: 'https://i.postimg.cc/SK026J8R/womens-leather-jacket-black.jpg',
       description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
-    }
+    },
+    {
+      itemName: "women's leather jacket brown",
+      price: 2699,
+      stock: 100,
+      color: 'brown',
+      gender: 'female',
+      imageSrc: 'https://i.postimg.cc/MGycdqbD/womens-leather-jacket-brown.jpg',
+      description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
+    },
+    {
+      itemName: "men's cotton trousers cream",
+      price: 599,
+      stock: 100,
+      color: 'white',
+      gender: 'male',
+      imageSrc: 'https://i.postimg.cc/wxW1WWJS/mens-cotton-trousers-cream.jpg',
+      description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
+    },
+    {
+      itemName: "men's cotton trousers tan",
+      price: 599,
+      stock: 100,
+      color: 'brown',
+      gender: 'male',
+      imageSrc: 'https://i.postimg.cc/RhbqNF4M/mens-cotton-trousers-tan.jpg',
+      description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
+    },
+    {
+      itemName: "men's track pants grey",
+      price: 199,
+      stock: 100,
+      color: 'grey',
+      gender: 'male',
+      imageSrc: 'https://i.postimg.cc/RZJJJcW2/mens-track-pants-grey.jpg',
+      description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
+    },
+    {
+      itemName: "women's palazzo pants azure",
+      price: 479,
+      stock: 100,
+      color: 'blue',
+      gender: 'female',
+      imageSrc: 'https://i.postimg.cc/HnLVY5GZ/womens-palazzo-pants-azure.jpg',
+      description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
+    },
+    {
+      itemName: "women's track pants black",
+      price: 249,
+      stock: 100,
+      color: 'black',
+      gender: 'female',
+      imageSrc: 'https://i.postimg.cc/vH9mLxgC/womens-track-pants-black.jpg',
+      description: `These comfortable socks from 'Happy Socks', are guaranteed to keep your feet satisfied.`
+    },
   ]
   productItems.forEach(item => {
     item.id = productItems.indexOf(item) + 1
